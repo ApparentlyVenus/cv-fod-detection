22ff605aa68b2e7f94f9e1fd33f865d502505ec1
Omar Dana <108975974+ApparentlyVenus@users.noreply.github.com>
2026-09-24 22:26:38 +0300

    Add model and launch files to setup.py

--- fod_ros/setup.py
+++ fod_ros/setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/models', ['models/best.onnx']),
         ('share/' + package_name + '/launch', ['launch/fod_detection.launch.py']),
     ],
     install_requires=['setuptools'],
